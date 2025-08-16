fda603e5a763bec98cf1532e986ecdd45d633630
StefBisti <stefbisti@gmail.com>
2025-08-16 13:19:16 +0300

    feat(tasks): starting

---
+++ lib/features/tasks/presentation/bloc/tasks_bloc.dart
@@ -0,0 +1,13 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'tasks_event.dart';
+part 'tasks_state.dart';
+
+class TasksBloc extends Bloc<TasksEvent, TasksState> {
+  TasksBloc() : super(TasksInitial()) {
+    on<TasksEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
