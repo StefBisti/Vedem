edd763a723bbdfb861fec9c2452bc0be06ed01c9
StefBisti <stefbisti@gmail.com>
2025-08-18 15:20:09 +0300

    app setup

---
+++ lib/core/error/exceptions.dart
@@ -0,0 +1,10 @@
+
+class ServerException implements Exception {
+  final String message;
+  const ServerException({required this.message});
+}
+
+class CacheException implements Exception {
+  final String message;
+  const CacheException({required this.message});
+}
